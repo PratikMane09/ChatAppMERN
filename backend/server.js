2842180a56a121d268cb9b95962fb823dd4b0a4e
PratikMane09 <Pratik09092001@gmail.com>
2024-04-21 21:39:59 +0530

    creating backend

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
+dotenv.config();
+app.use(express.json());
 app.use("/api/auth", authRoutes);
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
